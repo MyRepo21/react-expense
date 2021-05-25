466459561108e24977317df9cbbabb088e85683f
MyRepo21 <student2020@nym.hush.com>
2021-05-25 14:03:16 -0700

    Commit

--- src/components/EntryRow.js
+++ src/components/EntryRow.js
@@ -1,13 +1,32 @@
-import React from "react"
+import React from "react";
 
 function EntryRow() {
-    //const handleSubmit = (e) => {
-     //   e.preventDefault();  
-    return(
-        
-            <h2>Expense Table</h2>
-     
-    )
+  //const handleSubmit = (e) => {
+  //   e.preventDefault();
+  return (
+    <div>
+      <table className="expenseTbl">
+        <th>Date</th>
+        <th>Description</th>
+        <th>Amount</th>
+        <th>Vendor</th>
+        <tr>
+          <td>
+            <input type="date"></input>
+          </td>
+          <td>
+            <input type="description" placeholder="description"></input>
+          </td>
+          <td>
+            <input type="amount" placeholder="amount"></input>
+          </td>
+          <td>
+            <input type="vendor" placeholder="vendor"></input>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-export default EntryRow
+export default EntryRow;
